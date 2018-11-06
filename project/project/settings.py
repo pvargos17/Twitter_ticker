01011175537efbbf09946bedfd10d2535ab377fe
Patrikc Vargos <pvargos17@gmail.com>
2018-11-06 17:16:54 -0600

    adding views

--- project/project/settings.py
+++ project/project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tweet_find/templates/tweet_find')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- project/tweet_find/views.py
+++ project/tweet_find/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tweet_find index.")
+    signupform = UserCreationForm()
+    return render(request, "index.html", {"signupform": signupform})
 
 
-def profile(request):
-    if request.method == 'POST':
-        apiform = ApiCallForm(data=request.POST)
-        if apiform.is_valid():
-            base_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=[ticker]&apikey=demo"
-            key = apiform.cleaned_data['key']
-            response = requests.get(base_URL + key)
-            data = response.json()
-    else:
-        data = {}
-    apiform = ApiCallForm()
-    return render(request, 'profile.html', {'apiform': apiform, 'data': data})
+# def profile(request):
+#     if request.method == 'POST':
+#         apiform = ApiCallForm(data=request.POST)
+#         if apiform.is_valid():
+#             base_URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=[ticker]&apikey=demo"
+#             key = apiform.cleaned_data['key']
+#             response = requests.get(base_URL + key)
+#             data = response.json()
+#     else:
+#         data = {}
+#     apiform = ApiCallForm()
+#     return render(request, 'profile.html', {'apiform': apiform, 'data': data})
 
 # Create your views here.
 #NW6Y1YYJB06MMXP7
