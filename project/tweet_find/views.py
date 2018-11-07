fc602bb6e44a0d052188ae0fe9a152306903df74
Patrikc Vargos <pvargos17@gmail.com>
2018-11-07 01:27:39 -0600

    template/view added

--- project/tweet_find/views.py
+++ project/tweet_find/views.py
@@ -5,17 +5,26 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def index(request):
     if request.method == 'POST':
-        signupform = UserCreationForm(data=request.POST)
-        if signupform.is_valid():
-            username = signupform.cleaned_data['username']
-            password = signupform.cleaned_data['password1']
-            signupform.save() # shortcut to save user
-            return render(request, "index.html", {'signupform': signupform,
-                                                'username': username,
-                                                'pwd' : password})
-
+        if 'signup' in request.POST:
+            signupform = UserCreationForm(data=request.POST)
+            if signupform.is_valid():
+                signupform.save() # shortcut to save user
+                username = signupform.cleaned_data['username']
+                password = signupform.cleaned_data['password1']
+                authenticate(username=username, password=password)
+                return render(request, "home.html", {'username': username})
+        elif "signin" in request.POST:
+            signinform = AuthenticationForm(data=request.POST)
+            if signinform.is_valid():
+                username = signinform.cleaned_data['username']
+                password = signinform.cleaned_data['password']
+                authenticate(username=username, password=password)
+                return render(request, "home.html", {'username': username})
     signupform = UserCreationForm()
-    return render(request, "index.html", {"signupform": signupform})
+    signinform = AuthenticationForm()
+
+    return render(request, "index.html", {"signupform": signupform,
+                                        "signinform":signinform})
 
 
 # def profile(request):
