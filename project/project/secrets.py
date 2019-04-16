531938f92f75d7d88a7f12536779dfec9a2a331e
Patrikc Vargos <pvargos17@gmail.com>
2019-04-16 12:36:36 -0500

    key deleted

--- project/project/secrets.py
+++ project/project/secrets.py
@@ -4,4 +4,3 @@ CONSUMER_KEY = os.environ['FrIxnMTw6AlZmicChD1Mmcg7h']
 CONSUMER_SECRET = os.environ['bgVWxRgNzj93XDktc0PNUMAgC6LTUflarm5Djuv7sJItlySsA9']
 ACCESS_TOKEN = os.environ['710509107472678912-SDq2Rq0iNGcCRGVO8dwgft2Q4mZNyZG']
 ACCESS_SECRET = os.environ['tJmHPIpWrlnIB7pxKshxp2ttJc2B9u5OPLl1AqMBbIIpk']
-key = 'FrIxnMTw6AlZmicChD1Mmcg7h'
